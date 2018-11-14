3d6fe720c7069ef5f433f9ec6a68661a13803ee7
Anna Piirainen <anna.r.piirainen@helsinki.fi>
2018-11-14 20:51:46 +0200

    Lisätty Submission-olioiden luku ja tulostus

--- Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -1,8 +1,39 @@
 package ohtu;
 
+import java.util.ArrayList;
+
 public class Submission {
     private int week;
+    private int hours;
+    private ArrayList<Integer> exercises = new ArrayList<Integer>();
+    private String course;
 
+    public void setCourse(String course) {
+        this.course = course;
+    }
+    
+    public String getCourse() {
+        return course;
+    }
+
+
+    public void setExercises(ArrayList exercises) {
+        this.exercises = exercises;
+    }
+
+    public ArrayList getExercises() {
+        return exercises;
+    }
+
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+    
     public void setWeek(int week) {
         this.week = week;
     }
@@ -13,7 +44,12 @@ public class Submission {
 
     @Override
     public String toString() {
-        return ""+week;
+        String exercise = "";
+        for (int exe : exercises) {
+            exercise += Integer.toString(exe) + ", ";
+        }
+        return "" + this.course + ": viikko " + this.week + ", tuntia " + this.hours + ", " + 
+                this.exercises.size() + " tehtävää: " + exercise;
     }
     
 }
