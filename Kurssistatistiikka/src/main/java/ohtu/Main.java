01043ab24402867799d10181a025aadaac7ab9bd
Anna Piirainen <anna.r.piirainen@helsinki.fi>
2018-11-15 20:37:29 +0200

    Viikko3, tehtävä3

--- Kurssistatistiikka/src/main/java/ohtu/Main.java
+++ Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -1,9 +1,14 @@
 package ohtu;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import java.io.IOException;
 import java.util.ArrayList;
 import org.apache.http.client.fluent.Request;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
 
@@ -16,21 +21,56 @@ public class Main {
 
         String url = "https://studies.cs.helsinki.fi/courses/students/"+studentNr+"/submissions";
         String urlCourse = "https://studies.cs.helsinki.fi/courses/courseinfo";
-
+        String urlOhtu = "https://studies.cs.helsinki.fi/courses/ohtu2018/stats";
+        String urlRails = "https://studies.cs.helsinki.fi/courses/rails2018/stats";
+        
+        //Haetaan tiedot
         String bodyText = Request.Get(url).execute().returnContent().asString();
         String courseBodyText = Request.Get(urlCourse).execute().returnContent().asString();
+        String ohtuStats = Request.Get(urlOhtu).execute().returnContent().asString();
+        String railsStats = Request.Get(urlRails).execute().returnContent().asString();
         
 
-        System.out.println("json-muotoinen tehtävädata:");
-        System.out.println( bodyText );
+        //System.out.println("json-muotoinen tehtävädata:");
+        //System.out.println( bodyText );
         
-        System.out.println("json-muotoinen kurssidata:");
-        System.out.println(courseBodyText);
+        //System.out.println("json-muotoinen kurssidata:");
+        //System.out.println(courseBodyText);
+        
+
 
         Gson mapper = new Gson();
         Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
         Course[] courses = mapper.fromJson(courseBodyText, Course[].class);
         
+        JsonParser parser = new JsonParser();
+        JsonObject ohtuData = parser.parse(ohtuStats).getAsJsonObject();
+        JsonObject railsData = parser.parse(railsStats).getAsJsonObject();
+        
+        //Stats for ohtu
+        Object[] weeksOhtu =  ohtuData.keySet().toArray();
+        Integer studentsOhtu = 0;
+        Integer exerciseTotalOhtu = 0;
+        Double hoursTotalOhtu = 0.0;
+        for (Object week : weeksOhtu) {
+            JsonObject weeklyStats = ohtuData.getAsJsonObject((String) week);
+            studentsOhtu += Integer.parseInt(weeklyStats.get("students").toString());
+            exerciseTotalOhtu += Integer.parseInt(weeklyStats.get("exercise_total").toString());
+            hoursTotalOhtu += Double.parseDouble(weeklyStats.get("hour_total").toString());
+        }
+        
+        //Stats for rails
+        Object[] weeksRails =  railsData.keySet().toArray();
+        Integer studentsRails = 0;
+        Integer exerciseTotalRails = 0;
+        Double hoursTotalRails = 0.0;
+        for (Object week : weeksRails) {
+            JsonObject weeklyStats = railsData.getAsJsonObject((String) week);
+            studentsRails += Integer.parseInt(weeklyStats.get("students").toString());
+            exerciseTotalRails += Integer.parseInt(weeklyStats.get("exercise_total").toString());
+            hoursTotalRails += Double.parseDouble(weeklyStats.get("hour_total").toString());
+        }
+
         //Submissions for courses
         for (Course course : courses) {
             ArrayList<Submission> courseSub = new ArrayList<Submission>();
@@ -56,7 +96,17 @@ public class Main {
         System.out.println("Opiskelijanumero " + studentNr + "\n");
         for (Course course : courses) {
             if (!course.getSubmissions().isEmpty()) {
-                System.out.println(course);
+                System.out.println(course + "\n");
+                
+                if (course.getName().equals("ohtu2018")) {
+                    System.out.println("kurssilla yhteensä " + studentsOhtu + " palautusta, palautettuja tehtäviä " +
+                            exerciseTotalOhtu + " kpl, aikaa käytetty yhteensä " + hoursTotalOhtu + " tuntia\n");
+                } else if (course.getName().equals("rails2018")) {
+                    System.out.println("kurssilla yhteensä " + studentsRails + " palautusta, palautettuja tehtäviä " +
+                            exerciseTotalRails + " kpl, aikaa käytetty yhteensä " + hoursTotalRails + " tuntia\n");
+                } else {
+                    System.out.println("\n");
+                }
             }
         }
         
